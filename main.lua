1c3536aaeb5a2c6281d88fdca1b5c791e4fb2c65
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-10-22 17:42:25 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,14 @@
+if workspace:FindFirstChild("Watchdog") then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/InfiniteYieldPlus/main/watchdog.lua'))()
+else 
+end
+
+if game.PlaceId == 5163196050 then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/InfiniteYieldPlus/main/timmywars.lua'))
+    else
+end
+    
+    if game.PlaceId == 855499080 then
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/InfiniteYieldPlus/main/skywars.lua'))
+    else
+end
