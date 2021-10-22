28812442b0ff13b3d37dc077dae9be5fbd0acf02
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-10-22 17:45:30 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,11 +4,11 @@ else
 end
 
 if game.PlaceId == 5163196050 then
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/InfiniteYieldPlus/main/timmywars.lua'))
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/InfiniteYieldPlus/main/timmywars.lua'))()
     else
 end
     
     if game.PlaceId == 855499080 then
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/InfiniteYieldPlus/main/skywars.lua'))
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/nuldylan/InfiniteYieldPlus/main/skywars.lua'))()
     else
 end
